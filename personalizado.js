2c6c55a61b44908e9f38eceed62272f53dc22442
Lucas Rodrigues <lucas.rodrigues@seuvale.com>
2022-06-02 17:41:27 -0300

    function

---
+++ personalizado.js
@@ -0,0 +1,16 @@
+
+function valida() {
+    alert("senha incorreta");
+}
+
+function somar(a, b) {
+    var total = a + b;
+    alert("valor da soma:" + total);
+}
+
+function desconto(a , b){
+    var totaldesc  = a - b;
+    document.write("valor final com desconto:" + totaldesc);
+}
+desconto (97,15);
+    
